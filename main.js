38ab950ba3c90c9e0901b8b7d2e10507c866af5d
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-04-05 23:32:39 -0300

    Criado Função para Calcular Média

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('form-atividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt ="Emoji Celebrando" />';
 const imgReprovado = '<img src="./images/reprovado.png" alt ="Emoji Decepcionado" />';
+const atividades = [];
+const notas = [];
 
 var linhas = '';
 
@@ -9,11 +11,15 @@ form.addEventListener('submit', function(e) {
 
     adicionaLinha();
     atualizaTabela();
+    atualizaMediaFinal();
 })
 
 function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
     var linha = "<tr>";
     linha += `<td>${inputNomeAtividade.value}</td>`;
@@ -30,4 +36,19 @@ function adicionaLinha() {
 function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+    
+}
+
+function calculaMediaFinal() {
+    var somaDasNotas = 0;
+
+    for (var i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+
+    return somaDasNotas / notas.length;
 }
