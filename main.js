73e601e9839a68b65d1c6311f0495fd849302cb0
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-04-05 23:47:37 -0300

    Finalizado Função para Calcular a Média e Adicionado Verificação de Atividades com o Mesmo Nome

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const imgAprovado = '<img src="./images/aprovado.png" alt ="Emoji Celebrando" />
 const imgReprovado = '<img src="./images/reprovado.png" alt ="Emoji Decepcionado" />';
 const atividades = [];
 const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
 
 var linhas = '';
 
@@ -18,16 +21,20 @@ function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade:  ${inputNomeAtividade.value} já foi inserida`);
+    } else {
     atividades.push(inputNomeAtividade.value);
     notas.push(parseFloat(inputNotaAtividade.value));
 
     var linha = "<tr>";
     linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
     linha += "</tr>"
 
     linhas += linha;
+    }
 
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
@@ -40,7 +47,9 @@ function atualizaTabela() {
 
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
-    
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal() {
